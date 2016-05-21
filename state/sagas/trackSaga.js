cf6522a84660aac418eee6a8fbca5906fa234f1a
Dmitry Kurinskiy <name.alari@gmail.com>
2016-05-21 18:13:55 +0300

    tracking code

---
+++ state/sagas/trackSaga.js
@@ -0,0 +1,36 @@
+import {HISTORY_CHANGE} from "commons/containers/constants";
+import {takeEvery} from "redux-saga";
+import {select} from "redux-saga/effects";
+
+export default (trackingCode) => function* trackSaga() {
+
+  if (typeof window !== 'undefined' && !window.location.host.startsWith("dev.") && window.location.hostname !== "localhost") {
+    (function(window, document, script, url, r, tag, firstScriptTag) {
+      window['GoogleAnalyticsObject'] = r;
+      window[r] = window[r] || function() {
+            (window[r].q = window[r].q || []).push(arguments)
+          };
+      window[r].l = 1 * new Date();
+      tag = document.createElement(script),
+          firstScriptTag = document.getElementsByTagName(script)[0];
+      tag.async = 1;
+      tag.src = url;
+      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    })(
+        window,
+        document,
+        'script',
+        '//www.google-analytics.com/analytics.js',
+        'ga'
+    );
+
+    const ga = window.ga;
+
+    ga('create', trackingCode, 'auto');
+
+    yield* takeEvery([HISTORY_CHANGE], function*(action) {
+      ga('send', window.location.href)
+    })
+  }
+
+}
