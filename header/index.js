d05bc6bd23d21f21ea7fd6bf64b2e75bc2ab24b3
denisfl <delchyve@gmail.com>
2016-06-06 11:47:09 +0300

    update header

--- header/index.js
+++ header/index.js
@@ -4,17 +4,19 @@ import {FlatButton} from "material-ui";
 import Menu from "material-ui/svg-icons/navigation/menu";
 
 const AppHeader = ({children, title}) => {
-    return (
-        <div className="AppHeader">
-            <div className="AppHeader-menuBtn">
-                <FlatButton label="Menu" default={true} icon={<Menu />} id="triptych-open-menu"/>
-            </div>
-            <h2>{title}</h2>
-            <div className="AppHeader-container">
-                {children}
-            </div>
+  return (
+    <div className="AppHeader">
+      <div className="AppHeader-top">
+        <div className="AppHeader-menuBtn">
+          <FlatButton label="Menu" default={true} icon={<Menu />} id="triptych-open-menu"/>
         </div>
-    );
+        <h2>{title}</h2>
+      </div>
+      <div className="AppHeader-container">
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default AppHeader;
