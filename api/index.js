579634daba346dd979cf880f52965654de67f8c5
Dmitry Kurinskiy <name.alari@gmail.com>
2016-06-14 12:24:33 +0300

    as a repo in a repo?

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 export {API} from "./constants"
 export {createApiAction,createFailureAction,createRequestAction,createSuccessAction} from "./utils/apiActions"
 export request from "./services/request"
-export apiMiddleware from "./middleware/api/index"
+export apiMiddleware  from "./middleware/api/index"
