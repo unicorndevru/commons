599fb2106ecda1bf797c4dcd4e4c4511ccb8cd79
Dmitry Kurinskiy <name.alari@gmail.com>
2016-08-09 13:55:42 +0300

    partner view wip

--- viewport/components/InViewport.js
+++ viewport/components/InViewport.js
@@ -11,28 +11,45 @@ const checkVisible = (elm) => {
 
 export default React.createClass({
   getInitialState: () => ({
-    inViewport: false
+    inViewport: false,
+    wasInViewport: false,
+    wasOutViewport: false
   }),
 
-  handleViewportChange: function(e) {
+  handleViewportChange: function () {
     const inViewport = checkVisible(this.refs.inport)
-
+    
     if (this.state.inViewport !== inViewport) {
       this.setState({ inViewport })
-      const fn = inViewport ? "onIn" : "onOut"
+      const fn = inViewport ? 'onIn' : 'onOut'
       this.props[fn] && this.props[fn].apply()
+
+      if(!inViewport && !this.state.wasOutViewport) {
+        this.props.onFirstOut && this.props.onFirstOut()
+        this.setState({wasOutViewport: true})
+      }
+    }
+    if(inViewport && !this.state.wasInViewport) {
+      this.props.onFirstIn && this.props.onFirstIn()
+      this.setState({wasInViewport: true})
     }
   },
 
   componentDidMount: function() {
     window.addEventListener('wheel', this.handleViewportChange)
     window.addEventListener('resize', this.handleViewportChange)
+    window.addEventListener('touchmove', this.handleViewportChange)
+    window.addEventListener('scroll', this.handleViewportChange)
+    window.addEventListener('scrollEnd', this.handleViewportChange)
     this.handleViewportChange()
   },
 
   componentWillUnmount: function() {
     window.removeEventListener('wheel', this.handleViewportChange)
     window.removeEventListener('resize', this.handleViewportChange);
+    window.removeEventListener('touchmove', this.handleViewportChange);
+    window.removeEventListener('scroll', this.handleViewportChange);
+    window.removeEventListener('scrollEnd', this.handleViewportChange);
   },
 
   render: function() {
